f1186af92771d99cefde08a92110a22cfefd2d12
Allen Wu <allenwu@DN0a226655.SUNet>
2018-01-25 19:09:05 -0800

    fixed the code some and added a copy of the output to the root

--- cleaning/cleaning.py
+++ cleaning/cleaning.py
@@ -11,7 +11,7 @@ data = data.drop(columns=['Program'])
 data = data.dropna(subset=['BorrState'])
 
 # specify that NAICS and ZIP codes are categorical, need to fill these before casting
-data[['NaicsCode','BorrZip','CDC_Zip']] = data[['NaicsCode','BorrZip','CDC_Zip']].fillna(missing)
+data[['NaicsCode','BorrZip','CDC_Zip']] = data[['NaicsCode','BorrZip','CDC_Zip']].fillna("MISSING")
 data[['NaicsCode','BorrZip','CDC_Zip']] = data[['NaicsCode','BorrZip','CDC_Zip']].astype(str)
 
 # fill missing numerical data
@@ -36,9 +36,18 @@ zips = data[['BorrState','BorrZip','CDC_State','CDC_Zip']]
 for index, series in data.iterrows():
 	borr_bot, borr_top = zip_ref[series['BorrState']]
 	cdc_bot, cdc_top = zip_ref[series['CDC_State']]
-	if borr_bot > float(series['BorrZip']) or float(series['BorrZip']) > borr_top:
+	if(series['BorrZip'] == "MISSING"):
+		borr_zip = -1
+	else:
+		borr_zip = float(series['BorrZip']) 
+	if(series['CDC_Zip'] == "MISSING"):
+		cdc_zip = -1
+	else:
+		cdc_zip = float(series['CDC_Zip']) 
+
+	if borr_bot > borr_zip or borr_zip > borr_top:
 		zips.at[index,'BorrZip'] = "MISSING"
-	if borr_bot > float(series['CDC_Zip']) or float(series['CDC_Zip']) > borr_top:
+	if borr_bot > cdc_zip or cdc_zip > borr_top:
 		zips.at[index,'CDC_Zip'] = "MISSING"
 data.update(zips)
 
