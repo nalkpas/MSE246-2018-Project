714f993f6f21b1379d521a482b0133cd4426d32c
nalkpas <allenwu@DN0a237a6e.SUNet>
2018-02-24 23:51:19 -0800

    basic neural network model
    Trains, may have subtle bugs.
    
    To do:
    1. implement hyperparameter tuning, dev set check, test set validation
    2. clean up/make less hacky
    3. select inputs more carefully
    4. saving parameters after training

---
+++ neural network/defaultNN.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd 
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from torch.autograd import Variable
+
+FloatTensor = torch.FloatTensor
+LongTensor = torch.LongTensor
+ByteTensor = torch.ByteTensor
+
+class Net(nn.Module):
+	def __init__(self):
+		super(Net, self).__init__()
+		self.lin1 = nn.Linear(26, 10)
+		self.lin2 = nn.Linear(10, 20)
+		self.lin3 = nn.Linear(20, 40)
+		self.lin4 = nn.Linear(40, 20)
+		self.lin5 = nn.Linear(20, 2)
+
+	def forward(self, x):
+		x = F.relu(self.lin1(x))
+		x = F.relu(self.lin2(x))
+		x = F.relu(self.lin3(x))
+		x = F.relu(self.lin4(x))
+		x = F.relu(self.lin5(x))
+		return x.view(x.size(0),2)
+
+model = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+
+train_data = pd.read_csv("train.csv")
+train_data = train_data.drop(columns=["BorrName", "BorrStreet", "GrossApproval", "TermInMonths", 
+				 "ChargeOffDate", "GrossChargeOffAmount_Norm", "GrossChargeOffAmount_Norm", 
+				 "LoanStatus"])
+
+class DefaultDataset(Dataset):
+	def __init__(self, dataframe):
+		self.frame = dataframe
+
+	def __len__(self):
+		return len(self.frame)
+
+	def __getitem__(self,idx):
+		covariates = self.frame.drop(columns=["Default?"])
+		covariates = FloatTensor(covariates[idx:idx+1].as_matrix())
+		defaults = self.frame["Default?"]
+		defaults = LongTensor(defaults[idx:idx+1].as_matrix())
+		sample = {"X": covariates, "Y": defaults}
+		return sample
+
+train_set = DefaultDataset(train_data)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=100, shuffle=True, num_workers = 2)
+
+num_epochs = 2
+print_interval = 100
+for epoch in range(num_epochs):
+	running_loss = 0.
+	for i, batch in enumerate(train_loader):
+		X, Y = Variable(batch["X"]), Variable(batch["Y"]).squeeze(1)
+
+		optimizer.zero_grad()
+		predictions = model(X)
+		loss = criterion(predictions, Y)
+		loss.backward()
+		optimizer.step()
+
+		running_loss += loss.data[0]
+		if i % print_interval == print_interval - 1:
+			print('[%d, %5d] loss: %.3f' %
+				  (epoch + 1, i + 1, running_loss / print_interval))
+			running_loss = 0.
+print("done")
